38b140206a2a8415f4c73eeaead3a402e67d7006
sarahoco014 <soconnor2@outlook.com>
2023-07-25 12:42:48 +0100

    adds flight class

--- airport_lab/src/main/java/models/Flight.java
+++ airport_lab/src/main/java/models/Flight.java
@@ -1,14 +1,53 @@
 package models;
 
-import java.util.ArrayList;
+import interfaces.IID;
 
-public class Flight {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Flight implements IID {
 
     private String destination;
     private int flightId;
     private ArrayList<Passenger> passList;
 
+    public Flight(String destination, int flightId, ArrayList passList) {
+        this.destination = destination;
+        this.flightId = flightId;
+        this.passList = passList;
+    }
 
+    public String getDestination() {
+        return destination;
+    }
 
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId() {
+        this.flightId = geneID();
+    }
+
+    public ArrayList<Passenger> getPassList() {
+        return passList;
+    }
+
+    public void setPassList(ArrayList<Passenger> passList) {
+        this.passList = passList;
+    }
+
+    public void addPassList(Passenger passenger) {
+        this.passList.add(passenger);
+    }
+
+    public int geneID() {
+        Random rand = new Random();
+        return rand.nextInt(100_000, 1_000_000);
+    }
 
 }
